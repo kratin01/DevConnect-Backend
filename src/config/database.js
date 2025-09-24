a5fd2658862d143873091d039ea7ecb5199b9cb5
kratin01 <kratinaggarwal8750@gmail.com>
2025-09-24 19:30:23 +0530

    Fix MongoDB Connection Issue

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB_URI+"devTinder");
+    await mongoose.connect("mongodb+srv://kratin:FCZO1QtdWSLGNWAp@devtinder.gzw23ee.mongodb.net/devTinder");
     // console.log("MongoDB connected successfully!");
   } catch (err) {
     console.error("Error connecting to MongoDB:", err.message);
